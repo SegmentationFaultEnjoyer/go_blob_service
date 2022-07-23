20dccf70038073690bc2fff3d851a9f7b3aa765d
Mark <mwoyshvillo@gmail.com>
2022-07-23 18:33:09 +0300

    delete user

--- internal/model/user.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type User struct {
-	ID                int    `json:"id"`
-	Email             string `json:"email"`
-	EncryptedPassword string `json:"password,omitempty"`
-	Password          string `json:"-"`
-}
